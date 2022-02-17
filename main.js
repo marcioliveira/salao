9f6245caa47eee5d1a7272e9049c2b4280072f51
marcioliveira <marcioruelaoliveira@gmail.com>
2022-02-17 12:25:19 -0300

    :dizzy: Add scrollreveal in main.js

--- main.js
+++ main.js
@@ -41,3 +41,22 @@ const swiper = new Swiper(".swiper-container", {
   mousewheel: true,
   keyboard: true,
 });
+
+/* ScrollReveal: Mostra elementos quando rolar a pagina */
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
+
+scrollReveal.reveal(
+  `
+  #home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .Testimonials
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+);
