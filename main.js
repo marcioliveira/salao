e21ee314aa85e3764bbac6279abb95bc933b2236
marcioliveira <marcioruelaoliveira@gmail.com>
2022-02-17 11:10:56 -0300

    :heavy_plus_sign: Add swiperJs lib

--- main.js
+++ main.js
@@ -30,3 +30,14 @@ window.addEventListener("scroll", () => {
     header.classList.remove("scroll");
   }
 });
+
+/* Testimonials slider carousel Swiper */
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
