5db52897894a7a2abd5d1726e2d10bf883d4c7fb
marcioliveira <marcioruelaoliveira@gmail.com>
2022-02-19 21:01:46 -0300

    :recycle: Add 2 coluns in section testimonials 767px or larger
    3

--- main.js
+++ main.js
@@ -40,6 +40,12 @@ const swiper = new Swiper(".swiper-container", {
   },
   mousewheel: true,
   keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true,
+    },
+  },
 });
 
 /* ScrollReveal: Mostra elementos quando rolar a pagina */
