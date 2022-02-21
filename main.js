70983dd9926604c6788b523498b0c907ff72f377
marcioliveira <marcioruelaoliveira@gmail.com>
2022-02-21 12:56:28 -0300

    :lipstick: Add active link in the nav menu

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ for (const link of links) {
 }
 
 /* mudar o header da pagina quando der scroll */
-function changeHeaderWhenScroll() {
-  const header = document.querySelector("#header");
-  const navHeight = header.offsetHeight;
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
 
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     // se o scrool passar da altura do header
     header.classList.add("scroll");
@@ -69,9 +69,9 @@ scrollReveal.reveal(
 );
 
 /* Botão voltar para o topo */
-function backToTop() {
-  const backToTopButton = document.querySelector(".back-to-top");
+const backToTopButton = document.querySelector(".back-to-top");
 
+function backToTop() {
   if (window.scrollY >= 560) {
     backToTopButton.classList.add("show");
   } else {
@@ -79,8 +79,35 @@ function backToTop() {
   }
 }
 
+/* Menu ativo conforme a seção visível na pagina */
+const sections = document.querySelectorAll("main section[id]");
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  }
+}
+
 /* When rolar a pagina */
 window.addEventListener("scroll", () => {
   changeHeaderWhenScroll();
   backToTop();
+  activateMenuAtCurrentSection();
 });
