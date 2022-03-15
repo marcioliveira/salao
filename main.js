15d7f4ffe93455c7cfd254f85faa01af583a5970
marcioliveira <marcioruelaoliveira@gmail.com>
2022-03-14 21:19:40 -0300

    :sparkles: Create new scheduling

--- main.js
+++ main.js
@@ -111,3 +111,18 @@ window.addEventListener("scroll", () => {
   backToTop();
   activateMenuAtCurrentSection();
 });
+
+/* Modal */
+const Modal = {
+  open() {
+    //Abrir modal
+    //Adicionar a class active ao modal
+    document.querySelector(".modal-overlay").classList.add("active");
+    // alert("abri o modal");
+  },
+  close() {
+    //Fechar o modal
+    //Remover a class active do modal
+    document.querySelector(".modal-overlay").classList.remove("active");
+  },
+};
